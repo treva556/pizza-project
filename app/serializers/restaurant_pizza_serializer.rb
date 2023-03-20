5f598043d54eed147e22bce863234f91c875348d
treva556 <marktreva92@gmail.com>
2023-03-20 18:42:45 +0300

    routes

---
+++ app/serializers/restaurant_pizza_serializer.rb
@@ -0,0 +1,3 @@
+class RestaurantPizzaSerializer < ActiveModel::Serializer
+  attributes :id, :name, :address
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :destroy] do
+    resources :pizzas, only: [:index]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :pizzas, only: [:index]
+
+  resources :restaurant_pizzas, only: [:create]
 end
+
+=begin
+Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :show, :destroy]
+  resources :restaurant_pizzas, only: :create
+  resources :pizzas, only: :index
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+=end
