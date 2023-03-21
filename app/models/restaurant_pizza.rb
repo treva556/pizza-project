82140030607bfbc54b33181aebb3f6f4b813a733
treva556 <marktreva92@gmail.com>
2023-03-21 13:40:22 +0300

    changes

---
+++ app/models/restaurant_pizza.rb
@@ -0,0 +1,6 @@
+class RestaurantPizza < ApplicationRecord
+    belongs_to :pizza 
+    belongs_to :restaurant
+
+    validates :price, inclusion: 1..30
+end
---
+++ app/serializers/restaurant_pizza_serializer.rb
@@ -0,0 +1,3 @@
+# class RestaurantPizzaSerializer < ActiveModel::Serializer
+#   attributes :id, :pizza_id, :restaurant_id, :price
+# end
